bf9f5f1360900d4ceaefaafde26c0eb0e3b734ff
Nam Phuong Do <dpnam2112@gmail.com>
2025-05-05 13:29:54 +0700

    define udp request/response schemas

--- trackerclient/schemas.go
+++ trackerclient/schemas.go
@@ -7,18 +7,50 @@ const (
 	TrackerActionAnnounce uint32 = 0x00
 )
 
-// A generic payload structure for both UDP request/response.
-type UDPPayload interface {
-	Serialize() ([]byte, error)
-	GetAction() (uint32)
-	GetTxnID() (uint32)
+
+type TrackerUDPConnectRequest struct {
+	TxnID uint32
 }
 
-type udpConnectRequest struct {
-	action uint32
-	txnID uint32
+
+type AnnounceEvent uint32
+
+
+const (
+	AnnounceEventNone AnnounceEvent = 0
+	AnnounceEventCompleted AnnounceEvent = 1
+	AnnounceEventStarted AnnounceEvent = 2
+	AnnounceEventStopped AnnounceEvent = 3
+)
+
+
+type TrackerUDPIPV4AnnounceRequest struct {
+	TxnID int32
+	InfoHash int32
+	PeerID int32
+	Downloaded int32
+	Uploaded int32 
+	Left int32
+	Event AnnounceEvent
+	IPAddress int32
+	Key int32
+	NumWant int32
 }
 
-type udpIPV4AnnounceRequest struct {
-	connectionID uint32
+
+type TrackerUDPAnnounceResponse struct {
+	TxnID int32
+	Interval int32
+	Leechers int32
+	Seeders int32
+	PeerAddresses []struct {
+		Host string
+		Port uint16
+	}
+}
+
+
+type TrackerUDPErrorResponse struct {
+	TxnID int32
+	message string
 }
