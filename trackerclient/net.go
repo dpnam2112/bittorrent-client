d2763bcea536268cf5246c9451da2713e29fc00e
Nam Phuong Do <87682531+dpnam2112@users.noreply.github.com>
2025-05-16 08:05:15 +0700

    fix: connection ID mismatch; fix logic of calculating infohash (#17)

--- trackerclient/net.go
+++ trackerclient/net.go
@@ -11,16 +11,16 @@ type TrackerUDPClient struct {
 	Logger *slog.Logger
 }
 
-func (client *TrackerUDPClient) SendConnectRequest(trackerIP string, trackerPort int, readTimeout time.Duration) (*TrackerUDPConnectResponse, error) {
+func (client *TrackerUDPClient) SendConnectRequest(trackerIP net.IP, trackerPort int, readTimeout time.Duration) (*TrackerUDPConnectResponse, error) {
 	// remote address
 	raddr := net.UDPAddr{
 		Port: trackerPort,
-		IP: net.ParseIP(trackerIP),
+		IP: trackerIP,
 	}
 
 	conn, err := net.DialUDP("udp", nil, &raddr)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to open an UDP connection: %w", err)
+		return nil, fmt.Errorf("Failed to create an UDP socket to %s: %w", raddr.IP.String(), err)
 	}
 
 	conn.SetReadDeadline(time.Now().Add(readTimeout * time.Second))
@@ -28,7 +28,13 @@ func (client *TrackerUDPClient) SendConnectRequest(trackerIP string, trackerPort
 
 	// Generate a UDP connection request with transaction ID randomly generated.
 	connectRequest := CreateTrackerUDPConnectRequest(true)
-	client.Logger.Debug("Send a connect request to a tracker", "request_payload", connectRequest)
+	rawConnectRequest := connectRequest.Marshal()
+
+	client.Logger.Debug(
+		"Send a connect request to a tracker",
+		"request_payload", connectRequest,
+		"raw_payload", fmt.Sprintf("% x", rawConnectRequest),
+	)
 
 	_, err = conn.Write(connectRequest.Marshal())
 	if err != nil {
@@ -51,6 +57,11 @@ func (client *TrackerUDPClient) SendConnectRequest(trackerIP string, trackerPort
 		)
 	}
 
+	client.Logger.Debug(
+		"Received connect response from tracker",
+		"raw_payload", fmt.Sprintf("% x\n", buf[:n]),
+	)
+
 	connectResp, err := UnmarshalTrackerUDPConnectResponse(buf[:UDPConnectResponseSize])
 	if err != nil {
 		return nil, fmt.Errorf("Failed to unmarshal connect response: %w", err)
@@ -58,4 +69,47 @@ func (client *TrackerUDPClient) SendConnectRequest(trackerIP string, trackerPort
 
 	client.Logger.Debug("Received connect response from the tracker", "response_payload", connectResp)
 	return connectResp, nil
+}
+
+func (client *TrackerUDPClient) SendAnnounceRequest(
+	trackerIP net.IP,
+	trackerPort int,
+	readTimeout time.Duration,
+	r *TrackerUDPAnnounceRequest,
+) (*TrackerUDPAnnounceResponse, error) {
+	raddr := net.UDPAddr{
+		IP: trackerIP,
+		Port: trackerPort,
+	}
+
+	conn, err := net.DialUDP("udp", nil, &raddr)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to open an UDP socket to %s: %w", raddr.String(), err)
+	}
+
+	request := r.Marshal()
+	client.Logger.Debug("Send an announce request to the tracker", "raw_payload", fmt.Sprintf("% x\n", request))
+	_, err = conn.Write(request)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to send an UDP packet to %s: %w", raddr.String(), err)
+	}
+
+	// Max size of an IP packet is 65535 bytes
+	// An UDP packet is just a thin wrapper of an IP packet
+	responseBuf := make([]byte, 65535)
+	conn.SetReadDeadline(time.Now().Add(readTimeout * time.Second))
+	n, _, err := conn.ReadFromUDP(responseBuf)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read UDP announce response from %s: %w", raddr.String(), err)
+	}
+
+	client.Logger.Debug("Received announce response", "response_size", n, "raw_payload", fmt.Sprintf("% x", responseBuf[:n]))
+	announceResp, err := UnmarshalTrackerUDPAnnounceResponse(responseBuf[:n])
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read UDP announce response from %s: %w", raddr.String(), err)
+	}
+
+	return announceResp, nil
 }
